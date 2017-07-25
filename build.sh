9e50e5cef40796931d979393ef67184ccd5855d6
ahammedunni <ahammedunni@expressbase.com>
2017-07-25 23:11:26 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 #!/bin/bash
 set -ev
 dotnet restore ./ExpressBase.Web/ExpressBase.Web.csproj
-dotnet build -c ./ExpressBase.Web/ExpressBase.Web.csproj Release 
+dotnet build -c Release ./ExpressBase.Web/ExpressBase.Web.csproj
