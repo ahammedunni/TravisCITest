81ebda5dfc6adb43b6fb251b4ccec2bcb70fee3a
ahammedunni <ahammedunni@expressbase.com>
2017-07-26 11:23:15 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,10 @@ DOCKER_USERNAME=$1
 DOCKER_PASSWORD=$2
 
 # Create publish artifact
-dotnet publish -c Release -o src ./ExpressBase.Core.sln
+dotnet publish -c Release -o ./src ./ExpressBase.Web/ExpressBase.Web.csproj
 
 # Build the Docker images
-docker build -t ahammedunni/expressbase src/bin/Release/netcoreapp1.1/publish/.
+docker build -t ahammedunni/expressbase ./ExpressBase.Web/.
 docker tag ahammedunni/expressbase ahammedunni/expressbase:latest
 
 # Login to Docker Hub and upload images
