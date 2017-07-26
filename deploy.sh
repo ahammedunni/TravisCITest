43fe394328a60cf0e3d8f50b1b720f0714b8de09
ahammedunni <ahammedunni@expressbase.com>
2017-07-26 16:47:19 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 set -ev
 
-
-DOCKER_USERNAME=$1
-DOCKER_PASSWORD=$2  
+TAG=$1
+DOCKER_USERNAME=$2
+DOCKER_PASSWORD=$3
 
 # Create publish artifact
 dotnet publish -c Release -o ./src ./ExpressBase.Web/ExpressBase.Web.csproj
 
 # Build the Docker images
-docker build -t ahammedunni/expressbaseweb ./ExpressBase.Web/.
-docker tag ahammedunni/expressbaseweb ahammedunni/expressbase:latest
+docker build -t ahammedunni/expressbaseweb:$TAG ./ExpressBase.Web/.
+docker tag ahammedunni/expressbaseweb:$TAG ahammedunni/expressbase:latest
 
 # Login to Docker Hub and upload images
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+
+docker push ahammedunni/expressbase:$TAG
 docker push ahammedunni/expressbaseweb:latest
